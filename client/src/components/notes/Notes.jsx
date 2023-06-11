7e951c1dd9e096ab071bc5a3329b14c72dd11ac6
Stanley Francess <stanleyjanefrancess@gmail.com>
2023-06-11 13:08:37 -0700

    updated link
    updated navigation links

--- client/src/components/notes/Notes.jsx
+++ client/src/components/notes/Notes.jsx
@@ -3,6 +3,22 @@ import "./notes.css"
 
 export default function Notes() {
   return (
-    <div className='na-notes'>Notes</div>
+    <div className='na-notes'>
+      <div className='na-notes-wrapper'>
+        <h1 className='na-notes-header'>Emmanuel's Journal Entries</h1>
+        <p className='na-notes-numbers'>4/6 completed</p>
+        <div className="na-notes-row">
+          <div className="na-notes column">
+            notes
+          </div>
+          <div className="na-notes column">
+            notes
+          </div>
+          <div className="na-notes column">
+            notes
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,26 +1,19 @@
 import "./sidebar.css";
-import { RssFeed } from "@mui/icons-material";
 import { NavLink } from 'react-router-dom';
 import React, { useState } from 'react';
 import Data from "./sidebarData.json";
 
 const Sidebar = ({children}) => {
-  const[isOpen ,setIsOpen] = useState(false);
-    const toggle = () => setIsOpen (!isOpen);
-  
   return (
-      <div style={{flex: isOpen ? "2" : "0.6"}} className="na-sidebar">
+      <div className="na-sidebar">
          <div className="na-sidebar-wrapper">
-               <div style={{marginLeft: isOpen ? "50px" : "0px"}} className="na-sidebar-toggle">
-                  <RssFeed onClick={toggle}/>
-                </div>
              {
                  Data.map((item, index)=>(
                      <NavLink to={item.path} key={index} className="link" activeclassName="active">
                       <ul className="na-sidebar-list">
                         <li className="na-sidebar-list-item">
                         <span><img src={item.image} alt="icons" className="na-sidebar-icon"/></span>
-                        <span style={{display: isOpen ? "block" : "none"}} className="na-sidebar-list-text">{item.name}</span>
+                        <span className="na-sidebar-list-text">{item.name}</span>
                         </li>
                       </ul>
                      </NavLink>
