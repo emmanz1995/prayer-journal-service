2f31895cb020ec8682af896005eb049d4aa1e650
Stanley Francess <stanleyjanefrancess@gmail.com>
2023-06-11 12:30:09 -0700

    Client updated
    Client folder updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import PrayerWall from "./pages/Prayer Wall/PrayerWall";
+import Answered from "./pages/Answered/Answered";
+import Testimonies from "./pages/Testimonies/Testimonies";
+import Profile from "./pages/Profile/Profile";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<PrayerWall />} />
+          <Route path="/answered" element={<Answered />} />
+          <Route path="/testimonies" element={<Testimonies />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
